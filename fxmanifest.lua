0f05637ea4462a6072dc5e0e4c9747ae24a53ccd
unscalable <149246855+unscalable@users.noreply.github.com>
2025-04-18 14:01:56 +0200

    V1

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'YourName'
+description 'Edit Player Licenses Command'
+version '1.0.0'
+
+server_script 'server.lua'
+
+-- Make sure qb-core and oxmysql are running
+dependency 'qb-core'
+dependency 'oxmysql'
